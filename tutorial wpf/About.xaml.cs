302a942310abe58c74f04bbff6fc24b4ecbfe85a
Miske9 <124712586+Miske9@users.noreply.github.com>
2024-02-18 16:23:05 +0100

    o nama

--- tutorial wpf/About.xaml.cs
+++ tutorial wpf/About.xaml.cs
@@ -20,9 +20,36 @@ namespace tutorial_wpf
     /// </summary>
     public partial class About : Page
     {
+        // ViewModel koji će sadržavati podatke za binding
+        private AboutViewModel viewModel;
+
         public About()
         {
             InitializeComponent();
+
+            // Inicijalizacija ViewModel-a
+            viewModel = new AboutViewModel();
+
+            // Postavljanje DataContext-a na ViewModel
+            DataContext = viewModel;
+        }
+    }
+
+    // ViewModel klasa koja sadrži podatke za binding
+    public class AboutViewModel
+    {
+        public string Location { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+        public AboutViewModel()
+        {
+            // Inicijalizacija podataka
+            Location = "Hrvatska";
+            Address = "Svetvinčenat 988";
+            Phone = "+38597876765";
+            Fax = "051234";
         }
     }
 }
